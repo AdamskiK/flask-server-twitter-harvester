8fbc8ae163512056522ac4ee011fcbb42000b891
Kamil Adamski <adamski.ka@pg.com>
2020-06-14 18:06:28 +0200

    added loggging of errors for harvester

--- twitter/harvester.py
+++ twitter/harvester.py
@@ -1,3 +1,4 @@
+import logging.config
 import os
 import ssl
 import time
@@ -7,22 +8,27 @@ from listener import StreamListener
 from requests.exceptions import Timeout, ConnectionError
 from urllib3.exceptions import ReadTimeoutError
 
+logging.config.fileConfig('logging.conf')
+logger = logging.getLogger(__name__)
+
 
 class Harvester:
     """A wrapper class for the StreamListener class."""
-    def __init__(self, logging):
-        self.logging = logging
-    
+
+    def __init__(self):
+        pass
+
     def harvest(self, keywords):
         while True:
             try:
                 self._listen(keywords)
             except (tweepy.RateLimitError, Timeout, ssl.SSLError, ReadTimeoutError, ConnectionError, tweepy.TweepError) as e:
-                self.logging.info(f"Error {e} waiting ~1 min to continue")
+                logger.error(f"Error {e} waiting ~1 min to continue")
                 time.sleep(int(os.environ["ERROR_WAIT_TIME"]))
 
-    def _listen(self, keywords):
-        listener = StreamListener(keywords, logging=self.logging)
+    @staticmethod
+    def _listen(keywords):
+        listener = StreamListener(keywords)
         auth = listener.authorize(os.environ["CONSUMER_KEY"],
                                   os.environ["CONSUMER_SECRET"],
                                   os.environ["ACCESS_KEY"],
